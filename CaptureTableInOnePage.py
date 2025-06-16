2f36e2858b3f28151984311b087299f52ba1d05b
Hans Chen <hanschen31310@gmail.com>
2025-06-16 13:07:05 +0800

    06/16 新增檔案創建機制

--- CaptureTableInOnePage.py
+++ CaptureTableInOnePage.py
@@ -1,10 +1,10 @@
-from bs4 import BeautifulSoup
 import requests
-
 import pandas as pd
-import numpy as np
+import os
 
-def getTableData(soup, flag = False):
+from bs4 import BeautifulSoup
+
+def getTableData(soup, include_players_name = True):
     """_
         若已經獲取到表格標頭，則不再獲取
     """
@@ -43,30 +43,43 @@ def getTableData(soup, flag = False):
             players_data_text = [player.get_text(strip=True) for player in players_data]
             
             # 將球員資料轉換為二維列表
-            players_data_two_dim = [[players_text[i]] + players_data_text[i*(column_num-1):(i+1)*(column_num-1)] for i in range(row_num-1)]
-            
-            if flag:
-                return players_data_two_dim
+            if include_players_name:
+                players_data_two_dim = [[players_text[i]] + players_data_text[i*(column_num-1):(i+1)*(column_num-1)] for i in range(row_num-1)]
             else:
+                players_data_two_dim = [players_data_text[i*(column_num-1):(i+1)*(column_num-1)] for i in range(row_num-1)]
+            
                 return pd.DataFrame(players_data_two_dim, columns=deduped_headers)
         except Exception as e:
             print(f"處理表格數據時出現錯誤: {e}")
             
-            return [], []
+            return pd.DataFrame()
     else:
         print("未找到表格")
         
-        return [], []
+        return pd.DataFrame()
 
-def storeInCSV(df, filename):
-    df.to_csv("players_stats.csv", index=False)
+def storeInCSV(df, fileName, mode='w'):
+    """
+        將DataFrame儲存到CSV檔案。
+        mode='a' 表示附加寫入, mode='w' 表示覆蓋寫入。
+    """
+    if not os.path.exists(fileName) and mode == 'a':
+        mode = 'w'
+        print(f"檔案 {fileName} 不存在，將使用覆蓋模式寫入。")
     
-    print(f"數據已儲存到 {filename}")
+    df.to_csv(fileName, mode= mode, header= not(mode=='a' and os.path.exists(fileName)), index=False) # 若檔案不存在，則寫入標題
+    
+    print(f"數據已儲存到 {fileName}，模式: {mode}")
 
 if __name__ == "__main__":
     url = "https://www.mlb.com/stats/pitching/2003"
     soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    print(soup)
+    fileName = "players_stats.csv"
     
     table_data = getTableData(soup)
     
-    storeInCSV(table_data, "players_stats.csv")
+    storeInCSV(table_data, fileName= fileName, mode = 'a')
+    print("資料覆蓋寫入完成。")
+    storeInCSV(table_data, fileName= fileName, mode = 'w')
+    print("資料附加寫入完成。")
